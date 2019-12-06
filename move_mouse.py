a92bade29d04b83591e4e4a3cedb88f05ed95eb5
Isaac Stevens <isaacbstevens@gmail.com>
2019-12-06 10:45:06 -0500

    New Contact script adds

--- move_mouse.py
+++ move_mouse.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
   pyautogui.moveTo(500, 500, duration = 1)
   i = 0
   while i == 0:
-      pyautogui.moveRel(50, 0, duration = 1)
-      pyautogui.moveRel(-50, 0, duration = 1)
+      pyautogui.moveRel(50, 0, duration = .01)
+      pyautogui.moveRel(-50, 0, duration = .01)
   print("This is happening")
